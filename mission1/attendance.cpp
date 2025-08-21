01b35ddc9909e5e0e5442b225c0d79f44842390b
sk.lee <sk80.lee@gmail.com>
2025-08-21 12:53:40 +0900

    2차 리팩토링

--- mission1/attendance.cpp
+++ mission1/attendance.cpp
@@ -5,26 +5,31 @@
 #include <map>
 #include <algorithm>
 
-#define MAX_NUM		(100)
 
 using namespace std;
 
-struct Node {
-	string w;
-	string wk;
+#define MAX_NUM		(100)
+#define MAX_PLAYER	(100)
+
+enum {
+	MON = 0,
+	TUE = 1,
+	WED = 2,
+	THU = 3,
+	FRI = 4,
+	SAT = 5,
+	SUN = 6,
 };
+
+int attend_per_day[MAX_PLAYER][MAX_NUM];
+int points[MAX_PLAYER];
+
+string names[MAX_PLAYER];
+bool mandatory_training[MAX_PLAYER];
+
 
 map<string, int> player;
 int num_player = 0;
-
-//dat[사용자ID][요일]
-int dat[MAX_NUM][MAX_NUM];
-int points[MAX_NUM];
-int grade[MAX_NUM];
-string names[MAX_NUM];
-
-int wed[MAX_NUM];
-int weekend[MAX_NUM];
 
 int get_playerID(std::string& name)
 {
@@ -36,6 +41,15 @@ int get_playerID(std::string& name)
 	return player[name];
 }
 
+void update_trainingScore(int player_id, int day_index, int add_point) {
+
+	attend_per_day[player_id][day_index] += 1;
+	points[player_id] += add_point;
+	if ((day_index == 2) || (day_index == 5) || (day_index == 6)) {
+		mandatory_training[player_id] = true;
+	}
+}
+
 void analize_attendance(string name, string day) {
 	
 	int player_id = get_playerID(name);
@@ -45,67 +59,59 @@ void analize_attendance(string name, string day) {
 	add_point++;
 
 	if (day == "monday") {
-		day_index = 0;
+		//day_index = 0;
+		day_index = MON;
 	}
 	if (day == "tuesday") {
-		day_index = 1;
+		day_index = TUE;
 	}
 	if (day == "wednesday") {
-		day_index = 2;
+		day_index = WED;
 		add_point += 2;
-		wed[player_id] += 1;
 	}
 	if (day == "thursday") {
-		day_index = 3;
+		day_index = THU;
 	}
 	if (day == "friday") {
-		day_index = 4;
+		day_index = FRI;
 	}
 	if (day == "saturday") {
-		day_index = 5;
+		day_index = SAT;
 		add_point += 1;
-		weekend[player_id] += 1;
 	}
 	if (day == "sunday") {
-		day_index = 6;
+		day_index = SUN;
 		add_point += 1;
-		weekend[player_id] += 1;
 	}
-
 	//사용자ID별 요일 데이터에 1씩 증가
-	dat[player_id][day_index] += 1;
-	points[player_id] += add_point;
+	update_trainingScore(player_id, day_index, add_point);
 }
 
 void caculate_result() {
 
 	for (int player_id = 1; player_id <= num_player; player_id++) {
-		if (dat[player_id][2] > 9) {
+		if (attend_per_day[player_id][WED] > 9) {
 			points[player_id] += 10;
 		}
 
-		if (dat[player_id][5] + dat[player_id][6] > 9) {
+		if (attend_per_day[player_id][SAT] + attend_per_day[player_id][SUN] > 9) {
 			points[player_id] += 10;
 		}
+	}
+}
 
-		if (points[player_id] >= 50) {
-			grade[player_id] = 1;
-		}
-		else if (points[player_id] >= 30) {
-			grade[player_id] = 2;
-		}
-		else {
-			grade[player_id] = 0;
-		}
+void print_socre()
+{
+	for (int player_id = 1; player_id <= num_player; player_id++) {
 
 		cout << "NAME : " << names[player_id] << ", ";
 		cout << "POINT : " << points[player_id] << ", ";
 		cout << "GRADE : ";
 
-		if (grade[player_id] == 1) {
+		if (points[player_id] >= 50) {
 			cout << "GOLD" << "\n";
 		}
-		else if (grade[player_id] == 2) {
+		else if (points[player_id] >= 30) {
 			cout << "SILVER" << "\n";
 		}
 		else {
@@ -117,8 +123,7 @@ void caculate_result() {
 	std::cout << "Removed player\n";
 	std::cout << "==============\n";
 	for (int player_id = 1; player_id <= num_player; player_id++) {
-
-		if (grade[player_id] != 1 && grade[player_id] != 2 && wed[player_id] == 0 && weekend[player_id] == 0) {
+		if (points[player_id] < 30 && mandatory_training[player_id] == false) {
 			std::cout << names[player_id] << "\n";
 		}
 	}
@@ -134,4 +139,6 @@ int main() {
 	}
 
 	caculate_result();
+
+	print_socre();
 }
