c60f10780fd01e8e6d237ccd6ab57ac7d2d4ef41
sk.lee <sk80.lee@gmail.com>
2025-08-21 10:39:55 +0900

    method refactoring 1차

--- mission1/attendance.cpp
+++ mission1/attendance.cpp
@@ -5,6 +5,8 @@
 #include <map>
 #include <algorithm>
 
+#define MAX_NUM		(100)
+
 using namespace std;
 
 struct Node {
@@ -12,111 +14,98 @@ struct Node {
 	string wk;
 };
 
-map<string, int> id1;
-int id_cnt = 0;
+map<string, int> player;
+int num_player = 0;
 
 //dat[사용자ID][요일]
-int dat[100][100];
-int points[100];
-int grade[100];
-string names[100];
+int dat[MAX_NUM][MAX_NUM];
+int points[MAX_NUM];
+int grade[MAX_NUM];
+string names[MAX_NUM];
 
-int wed[100];
-int weeken[100];
+int wed[MAX_NUM];
+int weekend[MAX_NUM];
 
-void input2(string w, string wk) {
+int get_playerID(std::string& name)
+{
 	//ID 부여
-	if (id1.count(w) == 0) {
-		id1.insert({ w, ++id_cnt });
-
-		if (w == "Daisy") {
-			int debug = 1;
-		}
-
-		names[id_cnt] = w;
+	if (player.count(name) == 0) {
+		player.insert({ name, ++num_player });
+		names[num_player] = name;
 	}
-	int id2 = id1[w];
+	return player[name];
+}
 
-	//디버깅용
-	if (w == "Daisy") {
-		int debug = 1;
-	}
-
-
+void analize_attendance(string name, string day) {
+	
+	int player_id = get_playerID(name);
 	int add_point = 0;
-	int index = 0;
-	if (wk == "monday") {
-		index = 0;
-		add_point++;
+	int day_index = 0;
+
+	add_point++;
+
+	if (day == "monday") {
+		day_index = 0;
 	}
-	if (wk == "tuesday") {
-		index = 1;
-		add_point++;
+	if (day == "tuesday") {
+		day_index = 1;
 	}
-	if (wk == "wednesday") {
-		index = 2;
-		add_point += 3;
-		wed[id2] += 1;
-	}
-	if (wk == "thursday") {
-		index = 3;
-		add_point++;
-	}
-	if (wk == "friday") {
-		index = 4;
-		add_point++;
-	}
-	if (wk == "saturday") {
-		index = 5;
+	if (day == "wednesday") {
+		day_index = 2;
 		add_point += 2;
-		weeken[id2] += 1;
+		wed[player_id] += 1;
 	}
-	if (wk == "sunday") {
-		index = 6;
-		add_point += 2;
-		weeken[id2] += 1;
+	if (day == "thursday") {
+		day_index = 3;
+	}
+	if (day == "friday") {
+		day_index = 4;
+	}
+	if (day == "saturday") {
+		day_index = 5;
+		add_point += 1;
+		weekend[player_id] += 1;
+	}
+	if (day == "sunday") {
+		day_index = 6;
+		add_point += 1;
+		weekend[player_id] += 1;
 	}
 
 	//사용자ID별 요일 데이터에 1씩 증가
-	dat[id2][index] += 1;
-	points[id2] += add_point;
+	dat[player_id][day_index] += 1;
+	points[player_id] += add_point;
 }
 
-void input() {
-	ifstream fin{ "attendance_weekday_500.txt" }; //500개 데이터 입력
-	for (int i = 0; i < 500; i++) {
-		string t1, t2;
-		fin >> t1 >> t2;
-		input2(t1, t2);
-	}
+void caculate_result() {
 
-	for (int i = 1; i <= id_cnt; i++) {
-		if (dat[i][2] > 9) {
-			points[i] += 10;
+	for (int player_id = 1; player_id <= num_player; player_id++) {
+		if (dat[player_id][2] > 9) {
+			points[player_id] += 10;
 		}
 
-		if (dat[i][5] + dat[i][6] > 9) {
-			points[i] += 10;
+		if (dat[player_id][5] + dat[player_id][6] > 9) {
+			points[player_id] += 10;
 		}
 
-		if (points[i] >= 50) {
-			grade[i] = 1;
+		if (points[player_id] >= 50) {
+			grade[player_id] = 1;
 		}
-		else if (points[i] >= 30) {
-			grade[i] = 2;
+		else if (points[player_id] >= 30) {
+			grade[player_id] = 2;
 		}
 		else {
-			grade[i] = 0;
+			grade[player_id] = 0;
 		}
 
-		cout << "NAME : " << names[i] << ", ";
-		cout << "POINT : " << points[i] << ", ";
+		cout << "NAME : " << names[player_id] << ", ";
+		cout << "POINT : " << points[player_id] << ", ";
 		cout << "GRADE : ";
 
-		if (grade[i] == 1) {
+		if (grade[player_id] == 1) {
 			cout << "GOLD" << "\n";
 		}
-		else if (grade[i] == 2) {
+		else if (grade[player_id] == 2) {
 			cout << "SILVER" << "\n";
 		}
 		else {
@@ -127,14 +116,22 @@ void input() {
 	std::cout << "\n";
 	std::cout << "Removed player\n";
 	std::cout << "==============\n";
-	for (int i = 1; i <= id_cnt; i++) {
+	for (int player_id = 1; player_id <= num_player; player_id++) {
 
-		if (grade[i] != 1 && grade[i] != 2 && wed[i] == 0 && weeken[i] == 0) {
-			std::cout << names[i] << "\n";
+		if (grade[player_id] != 1 && grade[player_id] != 2 && wed[player_id] == 0 && weekend[player_id] == 0) {
+			std::cout << names[player_id] << "\n";
 		}
 	}
 }
 
 int main() {
-	input();
+	ifstream fin{ "attendance_weekday_500.txt" }; //500개 데이터 입력
+
+	for (int i = 0; i < 500; i++) {
+		string name, day;
+		fin >> name >> day;
+		analize_attendance(name, day);
+	}
+
+	caculate_result();
 }
